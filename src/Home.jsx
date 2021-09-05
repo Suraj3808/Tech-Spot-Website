7de1f4e111d0e3862390c710d0c4f7be1003975e
Suraj Singh <surajsinghjadone@gmail.com>
2021-09-06 02:42:32 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import web from "../src/Images/pic1.webp";
+import Common from "./Common";
+
+
+const Home = () => {
+
+
+    return (
+        <>
+
+            <Common
+                name='Grow Your Business With'
+                imgsrc={web}
+                visit="/service"
+                btnname="Get Started"
+            />
+
+        </>
+    );
+};
+export default Home;
