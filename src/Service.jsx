27394ba2eccd0ce16df8365bc317775b8d20afd5
Suraj Singh <surajsinghjadone@gmail.com>
2021-09-07 18:04:41 +0530

    final push

--- src/Service.jsx
+++ src/Service.jsx
@@ -13,7 +13,7 @@ const Service = () => {
             <div className="container-fluid mb-1">
                 <div className="row">
                     <div className="col-10 mx-auto">
-                        <div className="row gy-4">
+                        <div className="row">
                             {
                                 Sdata.map((val, index) => {
                                     return <Card className="pics"
